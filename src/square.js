9660deacf3d86ecf66120b1f6a47908241f02e54
sonyali03 <45471193+sonyali03@users.noreply.github.com>
2018-12-20 12:34:08 -0800

    Update square.js

--- src/square.js
+++ src/square.js
@@ -1,6 +1,24 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
+/**
+  *
+  * main() will be invoked when you Run This Action.
+  *
+  * @param OpenWhisk actions accept a single parameter,
+  *         which must be a JSON object.
+  *
+  * In this case, the params variable will look like:
+  *         { "message":  Webhook POST payload}
+  *
+  * @return which must be a JSON object.
+  *         It will be the output of this action.
+  *         returns commit history
+  *
+  */
 function main(params) {
 
-    console.log("Display GitHub Commit Details for GitHub repo test05: ", params.repository.url);
+    console.log("Display GitHub Commit Details for GitHub repo test04: ", params.repository.url);
     for (var commit of params.commits) {
         console.log(params.head_commit.author.name + " added code changes with commit message: " + commit.message);
     }
